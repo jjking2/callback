8e6e4c1c1f75f2ef0ad4b25c041754d01f778ea7
jjking2 <kouda.x.kumi@gmail.com>
2018-11-29 21:36:50 +0900

    Update a file

--- Callback.java
+++ Callback.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 /**
  * Java class for improving callback readability
- * @see github jjking2/callback
+ * @see jjking2/callback@github
  */
 public class Callback {
 
@@ -15,12 +15,23 @@ public class Callback {
 		Z
 	}
 	
+	/** map of callback tag and callback code */
 	private Map<Tag, Runnable> callbacks = new HashMap<Tag, Runnable>();
 	
+	/**
+	 * Set a pair of a callback-time selection tag and the runnable to be run if matched
+	 * @param tag selection tag
+	 * @param r callback handler
+	 */
 	public void setCallback(Tag tag, Runnable r) {
 		callbacks.put(tag, r);
 	}
-	
+
+	/**
+	 * get a runnable associated with the callback-time selection tag.
+	 * @param tag selection tag
+	 * @return callback handler
+	 */
 	public Runnable getCallback(Tag tag) {
 		return (Runnable)callbacks.get(tag);
 	}
